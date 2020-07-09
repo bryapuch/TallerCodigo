4ac0aed85319302bc43cf86acc783c6b3fedb8b9
ricdasal <56646823+ricdasal@users.noreply.github.com>
2020-07-09 18:07:41 -0500

    trabajo terminado

--- src/Users/Paciente.java
+++ src/Users/Paciente.java
@@ -1,12 +1,17 @@
 package Users;
 
 import java.util.Date;
+import java.util.LinkedList;
 
+import Receta.Receta;
 import main.Cita;
+import main.HistoriaClinica;
 
 public class Paciente extends Persona {
 	protected String email;
 	protected Cita cita;
+	protected HistoriaClinica historiaClinica;
+	protected LinkedList<Receta> recetas;
 		
 	public Paciente(String usuario, String clave, String nombre, String apellido, String cedula, String direccion,
 			Date fechaNac, String email) {
--- src/main/HistoriaClinica.java
+++ src/main/HistoriaClinica.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.util.ArrayList;
+
 public class HistoriaClinica {
+	protected int nro;
+	protected ArrayList<String> enfermendades;
+	protected ArrayList<String> alergias;
+	protected ArrayList<String> otros;
+	
+	
+	
+	
 
 }
