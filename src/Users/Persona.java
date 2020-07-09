bad0a30896f89a388f2e15d99934c14d1d9aaad9
ricdasal <56646823+ricdasal@users.noreply.github.com>
2020-07-09 10:28:48 -0500

    se creo los atributos de la clase persona

--- src/Users/Persona.java
+++ src/Users/Persona.java
@@ -1,5 +1,91 @@
 package Users;
 
+import java.util.Date;
+
 public class Persona {
+	protected String usuario;
+	protected String clave;
+	protected String nombre;
+	protected String apellido;
+	protected String cedula;
+	protected String direccion;
+	protected Date fechaNac;
+	
+	
+	public Persona(String usuario, String clave, String nombre, String apellido, String cedula, String direccion,
+			Date fechaNac) {
+		super();
+		this.usuario = usuario;
+		this.clave = clave;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cedula = cedula;
+		this.direccion = direccion;
+		this.fechaNac = fechaNac;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public boolean login() {
+		return true;
+	}
+	
+	public boolean logout() {
+		return true;
+	}
 
 }
